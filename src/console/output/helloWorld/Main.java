a2360eadff7a20d36daf0941432ceedc5da173ef
mkalnin <m.kalnin@gmail.com>
2023-03-17 15:36:29 +0300

    Add task 2 data types

--- src/console/output/helloWorld/Main.java
+++ src/console/output/helloWorld/Main.java
@@ -1,6 +1,9 @@
 package console.output.helloWorld;
 
 public class Main {
+    private static double PI = 3.14;
+    private static byte MAX_BYTE = 127;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
